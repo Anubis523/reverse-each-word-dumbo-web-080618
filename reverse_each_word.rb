b2315372d03798d20c845f6c47d60de58812cc38
Kevin Steele <kevindsteeleii@gmail.com>
2018-07-29 19:07:14 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,5 @@
 def reverse_each_word (phrase)
-  phrase.split(' ').map{ do 
-    |word| reverse_word(word)
-  end}
+  phrase.split(' ').map{|word| reverse_word(word)}.join
 end
 
 def reverse_word (word)
